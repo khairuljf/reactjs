0c08c1015e7513a6cd2623a0bfbad1a07f6f8ed0
khairul Islam <khairuljf@gmail.com>
2019-05-21 10:41:19 +0600

    Dynamic text change

--- indession-app/public/scripts/app.js
+++ indession-app/public/scripts/app.js
@@ -1,11 +1,40 @@
 'use strict';
 
-console.log('App.js is runing a');
+console.log('App.js is runing');
+
+var app = {
+    title: 'Indessin App',
+    subtitle: 'App Module list'
+};
 
 var template = React.createElement(
-  'p',
-  null,
-  'Does this change '
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title,
+        ' '
+    ),
+    React.createElement(
+        'h3',
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Add note'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Remove note'
+        )
+    )
 );
 
 var appRoot = document.getElementById('app');
--- indession-app/src/app.js
+++ indession-app/src/app.js
@@ -1,7 +1,20 @@
-console.log('App.js is runing a');
+console.log('App.js is runing');
 
 
-var template = <p>Does this change </p>;
+var app= {
+    title : 'Indessin App',
+    subtitle :'App Module list'
+}
+
+var template = (
+<div>
+    <h1>{app.title} </h1> 
+    <h3>{app.subtitle}</h3>
+    <ol>
+        <li>Add note</li>
+        <li>Remove note</li>
+    </ol>
+</div>);
 
 
 var appRoot = document.getElementById('app');
