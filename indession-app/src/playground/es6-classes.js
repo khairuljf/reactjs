d23b8bf8d3fe175d4b5cc78b135307bbd1aa4822
khairul Islam <khairuljf@gmail.com>
2019-05-21 14:55:39 +0600

    Overwriting main class function

--- indession-app/src/playground/es6-classes.js
+++ indession-app/src/playground/es6-classes.js
@@ -1,1 +1,34 @@
-console.log('ES6 classes')
+
+class Person{
+    constructor (name = 'Anonymus', age=0){
+        this.name= name
+        this.age = age
+    }
+    getDescription(){
+        return `Your name is ${this.name} and your age is ${this.age}`
+    }
+}
+
+class Student extends Person{
+        constructor(name, age, dept){
+            super(name, age)
+            this.dept =dept
+        }
+
+        hasDept(){
+            return !!this.dept
+        }
+        getDescription(){
+            return 'Testing'
+        }
+
+}
+
+
+const student = new Student('rubel', 26, 'Computer Science')
+
+console.log(student.hasDept())
+
+const other = new Student('khairul', 20)    
+
+console.log(other.hasDept())
