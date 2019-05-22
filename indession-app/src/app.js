911aa1df2c0ac957997c234add0695f80a3cf6b7
khairul Islam <khairuljf@gmail.com>
2019-05-22 09:45:43 +0600

    Send render data to another render

--- indession-app/src/app.js
+++ indession-app/src/app.js
@@ -1,9 +1,12 @@
 
 class IndesionApp extends React.Component{
     render(){
+        const title = 'Indecision'
+        const subtitle = 'Put your life in the hands of computer'
+
         return (
             <div>
-                <Header />
+                <Header title ={title} subtitle = {subtitle} />
                 <Action />
                 <Options />
                 <AddOption />
@@ -16,8 +19,8 @@ class Header extends React.Component{
     render(){
         return (
             <div>
-                <h1>Indecision</h1>
-                <h2>Put your life in the hands of computer</h2>
+                <h1>{this.props.title}</h1>
+                <h2>{this.props.subtitle}</h2>
             </div>
         )
     }
@@ -36,7 +39,7 @@ class Options extends React.Component{
                 Options component here
                 <Option />
             </div>
-            
+
         )
     }
 }
